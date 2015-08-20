25d7f5fed65c93be3b33a4adbf575acc9ff95db0
logan dougher <logan.dougher@gmail.com>
2015-08-20 13:56:05 -0500

    rough post test

--- app/mentions/test/unit/unit.js
+++ app/mentions/test/unit/unit.js
@@ -1,5 +1,7 @@
 var should = require("chai").should();
 //var nm = require('nodemailer');
+var request = require('supertest');
+
 
 describe('Mocha + Chai', function() {
   it('thruthiness', function () {
@@ -15,3 +17,29 @@ describe('#indexOf()', function () {
   });
 });
 
+describe('find username', function () {
+
+  beforeEach(function (done) {
+    var post = {
+
+    }
+    require('../../../../lib/mongo').connect(function(err, db){
+      db.collection.insert(username, function () {
+        done();
+      });
+    });
+  });
+  afterEach(function(done){
+    require('../../../../lib/mongo').connect(function(err, db){
+      username.remove({}, function () {
+      done();
+    });
+  });
+
+  it('should return username if mention added to user object', function (done) {
+    username.findByUsername(user.username, function (){
+      user.username.should.equal("buddythegirl");
+      done();
+    });
+  });
+});
