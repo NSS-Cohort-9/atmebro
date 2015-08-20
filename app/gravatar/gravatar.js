4328f090bcfa848d3684a394b825da1b1dba9007
Noah Yarian <noah.yarian@gmail.com>
2015-08-20 11:10:29 -0500

    renamed noah_simone folder to gravatar, fixed quotes, added .jpg extension to final URL, and set URL to a variable

--- app/gravatar/gravatar.js
+++ app/gravatar/gravatar.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var _ = require('lodash');
-var md5 = require("md5");
+var md5 = require('md5');
 
 module.exports = function(email) {
   var url = 'http://www.gravatar.com/avatar/';
-  return url + md5(_.trim(email).toLowerCase());
+  return url + md5(_.trim(email).toLowerCase()) + '.jpg';
 };
