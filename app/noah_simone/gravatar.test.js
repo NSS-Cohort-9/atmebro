7af5d4eeae226c389f84b84a9462d398f40f1dd6
Noah Yarian <noah.yarian@gmail.com>
2015-08-20 09:30:03 -0500

    added gravatar.js and passing tests

---
+++ app/noah_simone/gravatar.test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var expect = require('chai').expect;
+var getGravatar = require('./gravatar');
+
+describe('getGravatar()', function () {
+  it('should return the correct Gravatar URL when given a correctly formatted email address', function() {
+    var email = 'noah.yarian@gmail.com';
+    var output = getGravatar(email);
+    expect(output).to.equal('http://www.gravatar.com/avatar/d1827cb7215433baead881bb79520dd8');
+  });
+  it('should return the correct Gravatar URL when given an email address with leading and/or trailing whitespace', function() {
+    var email = ' noah.yarian@gmail.com ';
+    var output = getGravatar(email);
+    expect(output).to.equal('http://www.gravatar.com/avatar/d1827cb7215433baead881bb79520dd8');
+  });
+  it('should return the correct Gravatar URL when given an email address with uppercase letters', function() {
+    var email = 'Noah.Yarian@Gmail.com';
+    var output = getGravatar(email);
+    expect(output).to.equal('http://www.gravatar.com/avatar/d1827cb7215433baead881bb79520dd8');
+  });
+});
