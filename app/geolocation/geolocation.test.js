8a952878d5a5a0ac8538b59767a063b7a87cf851
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-08-21 15:48:54 -0500

    gets map object

--- app/geolocation/geolocation.test.js
+++ app/geolocation/geolocation.test.js
@@ -1,16 +1,26 @@
 var expect = require('chai').expect;
 var request = require('supertest');
-var geo = require('./geolocation.js')
+var geo = require('./geolocation.js');
+//var map = require('./map.js')
 
-describe('geolocation', function() {
-  describe.only('IP Address', function() {
-    it('should identify my IP address', function() {
+describe('geolocation', function () {
+  describe.only('IP Address', function () {
+    it('should identify my IP address', function () {
       var actual = require('./geolocation.js').getIP();
       request
         .post('/')
-        .end(function(err, res){
+        .end(function (err, res) {
           console.log('res');
         })
+    });
+  });
+});
+describe('Map', function () {
+  describe('constructor', function () {
+    it('should return an animal object', function () {
+      var animal = new Animal();
+      animal.should.be.an('object');
+      animal.should.be.an.instanceOf(Animal);
     });
   });
 });
--- www/clientgeo.js
+++ www/clientgeo.js
@@ -1,8 +1,29 @@
-$(document).ready(function() {
+$(document).ready(function () {
   navigator.geolocation.getCurrentPosition(showPosition);
+
+});
 
   function showPosition(position) {
     document.getElementById("lat").value = position.coords.latitude;
     document.getElementById("lng").value = position.coords.longitude;
-  }
-})
+
+    var pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    };
+    console.log(pos);
+      userMap(pos);
+    }
+
+  function userMap(pos) {
+    debugger;
+    var mapCanvas = document.getElementById('map-canvas'); //div in the html
+    var mapOptions = {
+        center: {lat: pos.lat, lng: pos.lng},
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      scrollwheel: false,
+    };
+    map = new google.maps.Map(mapCanvas, mapOptions);
+    console.log(map);
+  };
