becdc674b60c24a136222dc8fc47791fe2ec4373
Brien Pafford <bcpaff@gmail.com>
2015-08-21 13:31:51 -0500

    actually added file

---
+++ app/mentions/mentions.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var expect 		= require('chai').expect;
+
+var Mentions 	= require('./mentions');
+var mongo 		= require('../../lib/mongo/');
+
+
+// Test Chai
+
+describe('Mocha + Chai', function() {
+  it('thruthiness', function () {
+    true.should.equal(true);
+    false.should.equal(false);
+  });
+});
+
+describe('Mentions', function () {
+	var seededPosts;
+
+
+
+
+
+
+
+
+
+
+})
