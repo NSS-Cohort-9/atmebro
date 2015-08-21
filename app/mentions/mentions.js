d34e5ce72427f5a3247369aa788d425cd5607c3d
Brien Pafford <bcpaff@gmail.com>
2015-08-21 11:22:49 -0500

    Added Unit test for returning post object from collection

---
+++ app/mentions/mentions.js
@@ -0,0 +1,14 @@
+var express = require('express');
+//var router  = require('router');
+//var path    = require('path');
+
+var postObj = {
+  text: "hola",
+  mention: "LDougher",
+  username: "buddy",
+  date: "Thu Aug 20 2015 14:34:20 GMT-0500 (CDT)",
+  geolocation: "nashville",
+  hidden: false
+};
+
+module.exports = postObj;
---
+++ app/mentions/routes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var tests = require('./test/unit/unit');
+
+
+
+module.exports = router;
