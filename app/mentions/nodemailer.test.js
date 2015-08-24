1e13e4c778301f0e187fb6eeb18693677de52f79
logan dougher <logan.dougher@gmail.com>
2015-08-24 11:54:27 -0500

    moved nm to describe block in nodemailer.test, cleaned up console logs

--- app/mentions/nodemailer.test.js
+++ app/mentions/nodemailer.test.js
@@ -4,7 +4,6 @@ var nodemailer = require('nodemailer');
 var expect    = require('chai').expect;
 var should    = require('chai').should();
 var nodemailerJS = require('./nodemailer');
-var mailOptions = require('./nodemailer');
 
 describe('nodemailer unit tests', function () {
   var nm, transport;
@@ -16,19 +15,16 @@ describe('nodemailer unit tests', function () {
         cb();
       }
     };
+    nm = nodemailer.createTransport(transport);
   });
 
   it('should create Nodemailer transport object', function() {
-    nm = nodemailer.createTransport({});
     expect(nm).to.exist;
   });
 
   it('should create mailOptions object', function (done) {
 
-    var mailOptions;
-
     expect(mailOptions.subject).to.equal('hello');
-    console.log(mailOptions.subject);
     done();
   });
 
