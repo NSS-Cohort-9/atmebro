4750fb88eed32f97e7f8bad5cf2911d9315af5fa
Jordan Thompson <jordanthomp81@gmail.com>
2015-08-27 00:36:12 -0500

    added testing for admin toggle function

---
+++ app/admin/admin.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+var User = require('../user/User');
+var mongo = require('../../lib/mongo/');
+
+describe('User', function () {
+
+	before(function (done) {
+		mongo.connect(function () {
+			var user = {_id: 'foo', admin_status: false};
+
+			User.collection.insertOne(user, done);
+		});
+	});
+
+	after(User.dropCollection);
+
+	describe('.toggleAdminStatus()', function () {
+		User.findByUserName('foo', function (err, user) {
+	        if (err) { throw err; }
+	        expect(user.admin_status).to.eql('{admin_status: true}');
+	        done();
+	  	});
+	});
+
+});
